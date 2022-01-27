1cd23d6195f073ad6ffa0747877b3c1175b044c0
mramada22 <mramada@ucsd.edu>
2022-01-26 20:44:17 -0800

    Adding Tests to Markdown Parse for other files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,9 +14,33 @@ public class MarkdownParseTest {
 
     @Test
     public void testMarkdownParse() throws IOException{
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("Test-file.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals(List.of("https://something.com","some-page.html"),links);
+    }
+
+    @Test
+    public void testMarkdownParse2() throws IOException{
+        Path fileName = Path.of("Test-file2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("something.in"), links);
+    }
+
+    @Test
+    public void testMarkdownParse3() throws IOException{
+        Path fileName = Path.of("Test-file3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://someurl.com","https://another.com","https://oops.com"), links);
+    }
+
+    @Test
+    public void testMarkdownParse4() throws IOException{
+        Path fileName = Path.of("Test-file4.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of(), links);
     }
 }
